4fcd32d96a51ec0f6414f81f82e14e73c1fb9ff2
LegJo <106883191+LegJo@users.noreply.github.com>
2022-11-06 02:11:37 +0100

    Update Carte.c

--- Carte.c
+++ Carte.c
@@ -9,9 +9,9 @@
 #define TAILLE_AVE_MAP 5
 #define TAILLE_LIL_MAP 10
 
-#define TAUX_DIFF_EZ 0.2
-#define TAUX_DIFF_NORMAL 0.4
-#define TAUX_DIFF_HARD 0.7
+#define TAUX_DIFF_EZ 0.8
+#define TAUX_DIFF_NORMAL 1
+#define TAUX_DIFF_HARD 1.8
 
 #define REP_CHARACTER 'P'
 #define REP_BONUS 'B'
@@ -35,7 +35,7 @@
 
 int RNG(int min, int max)
 {
-    return ( ( rand() % (max + min - 1) ) + min );
+    return ( ( rand() % (max - min + 1) ) + min );
 }
 
 void UnallocMap(int** matrice_Map, int int_mapSize)
@@ -67,55 +67,67 @@ int** AllocMap(int int_mapSize)
     return (matrice_Map);
 }
 
-int** GenerateMap(int** matrice_Map, int int_mapSize) //Work In Progress
+int** GenerateMap(int** matrice_Map, int int_mapSize, float float_diffRate) //Work In Progress
 {
+    int int_nbObstacles = int_mapSize * float_diffRate ;
+    int int_nbBonus = ( int_mapSize / 12.5 ) * ( 3 - float_diffRate );
+
     int int_maxCoord = int_mapSize - 1 ;
-    int int_qrtCoord = int_mapSize / 4 ;
+    int int_qrtCoord = int_mapSize/ 4 ;
     int int_rdmRow;
     int int_rdmCol = int_mapSize;
-    int rdm = RNG(1,4) ;                
-    switch ( rdm ) //Placement du personnages aleatoirement dans un des 4 coins de la map, et placement du drapeau en consequence semi-aléatoirement
+
+    srand(time(NULL)); //init RNG
+         
+    switch ( RNG(1,4) ) //Placement du personnages aleatoirement dans un des 4 coins de la map, et placement du drapeau en consequence semi-aléatoirement
     {
     case 2:
         matrice_Map[int_maxCoord][0]= REP_CHARACTER;
         int_rdmRow = RNG( 0, int_qrtCoord );
-        while ( int_rdmCol >= int_maxCoord + ( int_maxCoord - int_qrtCoord - int_rdmRow ) )
-        {
-            int_rdmCol = RNG( int_maxCoord - int_qrtCoord, int_maxCoord );
-        }
+        int_rdmCol = RNG( int_maxCoord - int_qrtCoord + int_rdmRow - int_qrtCoord, int_maxCoord);
         break;
     case 3:
         matrice_Map[0][int_maxCoord]= REP_CHARACTER;
-        int_rdmRow = RNG( int_qrtCoord, int_maxCoord );
-        while ( int_rdmCol <= int_rdmRow - int_qrtCoord )
-        {
-            int_rdmCol = RNG( 0, int_qrtCoord );
-        }
+        int_rdmRow = RNG( int_maxCoord - int_qrtCoord, int_maxCoord );
+        int_rdmCol = RNG( 0, int_rdmRow - int_maxCoord + int_qrtCoord );
         break;
     case 4:
         matrice_Map[int_maxCoord][int_maxCoord]= REP_CHARACTER;
         int_rdmRow = RNG( 0, int_qrtCoord );
-        while ( int_rdmCol <= int_maxCoord - int_qrtCoord - int_rdmRow + 1)
-        {
-            int_rdmCol = RNG( 0, int_qrtCoord );
-        }
+        int_rdmCol = RNG( 0, int_qrtCoord - int_rdmRow );
         break;
     default:
         matrice_Map[0][0]= REP_CHARACTER;
-        int_rdmRow = RNG( int_qrtCoord, int_maxCoord );
-        while ( int_rdmCol >= int_maxCoord - int_rdmRow + int_qrtCoord + 1)
-        {
-            int_rdmCol = RNG( int_qrtCoord, int_maxCoord );
-        }
+        int_rdmRow = RNG( int_maxCoord - int_qrtCoord, int_maxCoord );
+        int_rdmCol = RNG( int_maxCoord - int_rdmRow + int_qrtCoord, int_maxCoord );
         break;
     } 
-    //printf("%d: %d,%d\n", rdm ,int_rdmRow,int_rdmCol);
     matrice_Map[int_rdmRow][int_rdmCol] = REP_END ;
 
-    for( int i = 0 ; i<int_mapSize; i++) { 
+    // printf("o,b : %d,%d", int_nbObstacles, int_nbBonus);
+
+    while( int_nbObstacles > 0 ) { //Placement des Obstacles
+        int_rdmRow = RNG(0,int_maxCoord);
+        int_rdmCol = RNG(0,int_maxCoord);
+
+        if( matrice_Map[int_rdmRow][int_rdmCol] == NULL ) {
+             matrice_Map[int_rdmRow][int_rdmCol] = REP_OBSTACLE ;
+             int_nbObstacles--;
+        }
+    }
+    while( int_nbBonus > 0 ) { //Placement des Bonus
+        int_rdmRow = RNG(0,int_maxCoord);
+        int_rdmCol = RNG(0,int_maxCoord);
+
+        if( matrice_Map[int_rdmRow][int_rdmCol] == NULL ) {
+             matrice_Map[int_rdmRow][int_rdmCol] = REP_BONUS ;
+             int_nbBonus--;
+        }
+    }
+
+    for( int i = 0 ; i<int_mapSize; i++) { //fill the left empty space 
        for( int j = 0 ; j<int_mapSize ; j++){
-            if( matrice_Map[i][j] == NULL )
-            {
+            if( matrice_Map[i][j] == NULL ){
                 matrice_Map[i][j] = '0';
             }
         }
@@ -145,8 +157,6 @@ void DebugDisplayMap(int** matrice_Map, int int_mapSize)
 }
 
 
-
-
 void DisplayMap(int** matrice_Map, int int_mapSize)
 {
     int int_curseur = 0;
@@ -157,28 +167,28 @@ void DisplayMap(int** matrice_Map, int int_mapSize)
             switch (matrice_Map[int_curseurBis][int_curseur])
             {
             case 'P' :
-                printf("%s%s %s %s%s", CORNER_LEFT, HORIZONTAL, PERSONNAGE,HORIZONTAL, CORNER_RIGHT);
+                printf("%s%s%s%s%s", CORNER_LEFT, HORIZONTAL, PERSONNAGE,HORIZONTAL, CORNER_RIGHT);
             break;
             case 'C' :
                 if (RNG(1,2)==1)
                 {
-                    printf("%s%s %s %s%s", CORNER_LEFT, HORIZONTAL,OBSTACLE_ABEILLE, HORIZONTAL,CORNER_RIGHT);
+                    printf("%s%s%s%s%s", CORNER_LEFT, HORIZONTAL,OBSTACLE_ABEILLE, HORIZONTAL,CORNER_RIGHT);
                 }else
                 {
-                    printf("%s%s %s %s%s", CORNER_LEFT, HORIZONTAL,OBSTACLE_ARBRE,HORIZONTAL, CORNER_RIGHT);
+                    printf("%s%s%s%s%s", CORNER_LEFT, HORIZONTAL,OBSTACLE_ARBRE,HORIZONTAL, CORNER_RIGHT);
                 }
             break;
             case 'B' :
-                printf("%s%s %s %s%s", CORNER_LEFT, HORIZONTAL, BONUS,HORIZONTAL, CORNER_RIGHT);
+                printf("%s%s%s%s%s", CORNER_LEFT, HORIZONTAL, BONUS,HORIZONTAL, CORNER_RIGHT);
             break;
             case 'D' :
-                printf("%s%s %s %s%s", CORNER_LEFT, HORIZONTAL, END,HORIZONTAL, CORNER_RIGHT);
+                printf("%s%s%s%s%s", CORNER_LEFT, HORIZONTAL, END,HORIZONTAL, CORNER_RIGHT);
             break;
             case 'S' :
-                printf("%s%s %s %s%s", CORNER_LEFT, HORIZONTAL, START, HORIZONTAL,CORNER_RIGHT);
+                printf("%s%s%s%s%s", CORNER_LEFT, HORIZONTAL, START, HORIZONTAL,CORNER_RIGHT);
             break;
             default:
-                printf("%s%s %s %s%s", CORNER_LEFT, HORIZONTAL, VIDE, HORIZONTAL,CORNER_RIGHT);
+                printf("%s%s%s%s%s", CORNER_LEFT, HORIZONTAL, VIDE, HORIZONTAL,CORNER_RIGHT);
             break;
             }
             int_curseur++;
@@ -188,20 +198,58 @@ void DisplayMap(int** matrice_Map, int int_mapSize)
     }
 }
 
+float ChooseDifficulty()
+{
+    int int_scanDiff;
+    printf("Choose a Difficulty :\n1: Easy\n2: Normal\n3: Hard\n>");
+    scanf("%d", &int_scanDiff);
+    switch (int_scanDiff) 
+    {
+    case 1:
+        return (TAUX_DIFF_EZ);
+        break;
+    case 3:
+        return (TAUX_DIFF_HARD);
+        break;
+    default:
+        return (TAUX_DIFF_NORMAL);
+        break;
+    }
+}
 
+int ChooseMapSize()
+{
+    int int_scanMapS;
+    printf("Choose the map size :\n1: Little\n2: Average\n3: Big\n>");
+    scanf("%d", &int_scanMapS);
+    switch (int_scanMapS)
+    {
+    case 1:
+        return (TAILLE_LIL_MAP);
+        break;
+    case 3:
+        return (TAILLE_BIG_MAP);
+        break;
+    default:
+        return (TAILLE_AVE_MAP);
+        break;
+    }
+}
 
 int main(void)
 {
-    srand(time(NULL)); //initialisation d'un tier pour la RNG
-    int int_mapSize = TAILLE_AVE_MAP;
+   
+    float float_diffRate = ChooseDifficulty();
+    int int_mapSize = ChooseMapSize();
+
     int **matrice_Map = AllocMap(int_mapSize);
-    matrice_Map = GenerateMap(matrice_Map, int_mapSize);
-    
-    DisplayMap(matrice_Map, int_mapSize);
-    //DebugDisplayMap(matrice_Map, int_mapSize);  
-    
+    matrice_Map = GenerateMap(matrice_Map, int_mapSize, float_diffRate);
+
+    //DebugDisplayMap(matrice_Map, int_mapSize);
+    DisplayMap(matrice_Map, int_mapSize);  
     UnallocMap(matrice_Map, int_mapSize);
-    
-    
+
     return 0;
 }
+
+
