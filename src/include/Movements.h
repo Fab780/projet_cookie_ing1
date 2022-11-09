ec0f1f51b5595866b1d7fa6f16062b40904cc45c
adamou02 <42783707+adamou02@users.noreply.github.com>
2022-11-10 00:03:05 +0100

    Update Movements.h

--- src/include/Movements.h
+++ src/include/Movements.h
@@ -7,4 +7,51 @@
 #include "Miscellanous.h"
 
 
+
+
+#include <stdio.h>
+#include <termios.h>
+
+#define UP_ARROW    259
+#define LEFT_ARROW  260
+#define DOWN_ARROW  258
+#define RIGHT_ARROW 261
+
+static struct termios old, current;
+
+int main() {
+    FILE* flux = stdin;
+    tcgetattr(0, &old); /* grab old terminal i/o settings */
+    current = old; /* make new settings same as old settings */
+    current.c_lflag &= ~ICANON; /* disable buffered i/o */
+    current.c_lflag &= ~ECHO; /* set no echo mode */
+    tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
+
+    int ch;
+    ch = getc(flux);
+    printf("Vous avez appuyé sur la touche %d.\n", ch);
+
+    switch(ch) {
+    case UP_ARROW:
+        printf("UP WAS PRESSED\n");
+        break;
+    case DOWN_ARROW:
+        printf("DOWN WAS PRESSED\n");
+        break;
+    case LEFT_ARROW:
+        printf("LEFT WAS PRESSED\n");
+        break;
+    case RIGHT_ARROW:
+        printf("RIGHT WAS PRESSED\n");
+        break;
+    default:
+        printf("SOME OTHER SCROLL KEY PRESSED: %d\n", ch);
+        break;
+    }
+    return 0;
+}
+
+
+
+
 #endif
