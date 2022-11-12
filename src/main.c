aba41e1d426b1540fd3986a81e948351d8560b04
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2022-11-12 12:00:39 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -7,7 +7,8 @@ int main(void)
     float float_diffRate = ChooseDifficulty();
     int int_mapSize = ChooseMapSize();
     PlayerInfo s_playerInfo_player = SetupPlayer(); 
-    int int_energy = InitEnergy(int_mapSize, &s_playerInfo_player);
+    s_playerInfo_player.int_energy = InitEnergy(int_mapSize, s_playerInfo_player.int_energy);
+    int int_victory = 0;
 
     int **matrice_Map = AllocMatriceMap(int_mapSize);
     matrice_Map = InitMatriceMap(matrice_Map, int_mapSize);
@@ -15,17 +16,23 @@ int main(void)
 
     srand(time(NULL)); //init RNG
     matrice_Map = GenerateMap(matrice_Map, int_mapSize, float_diffRate, &s_playerInfo_player);
-    matrice_Distance = Generatematrice_Distance(int_mapSize, matrice_Distance);
+    // matrice_Distance = Generatematrice_Distance(int_mapSize, matrice_Distance);
 
     DisplayMap(matrice_Map, int_mapSize);
-    DisplayMatriceDistance(int_mapSize, matrice_Distance);
+    // DisplayMatriceDistance(int_mapSize, matrice_Distance);
     printf("Votre energie : %d\n",s_playerInfo_player.int_energy);
-    printf("Vos coordonnées: %d , %d\n",s_playerInfo_player.int_x,s_playerInfo_player.int_y);
 
-    // matrice_Map = AfterMovement(matrice_Map, 4, 5, &s_playerInfo_player, int_mapSize);
-    // DisplayMap(matrice_Map, int_mapSize); 
-    // printf("Votre energie : %d\n",s_playerInfo_player.int_energy);
-    // printf("Vos coordonnées: %d , %d\n",s_playerInfo_player.int_x,s_playerInfo_player.int_y);
+    int key_pressed;
+    int int_wanted_x;
+    int int_wanted_y;
+    while(int_victory == 0){
+        key_pressed = ListenKeyboard();
+        ChangePosition(key_pressed, &int_wanted_x, &int_wanted_y, &s_playerInfo_player); 
+        matrice_Map = AfterMovement(matrice_Map, int_wanted_x, int_wanted_y, &s_playerInfo_player, int_mapSize, &int_victory);
+        printf("\n"); 
+        DisplayMap(matrice_Map, int_mapSize); 
+        printf("Votre energie : %d\n",s_playerInfo_player.int_energy);
+    }
     // DebugDisplayMap(matrice_Map, int_mapSize);  
 
     UnallocMatriceMap(matrice_Map, int_mapSize);
