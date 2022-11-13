ac2abbb084e726620dba7a79f587002747ae637e
LegJo <106883191+LegJo@users.noreply.github.com>
2022-11-13 23:23:09 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -15,11 +15,19 @@ int main(void)
     int*** matrice_Distance= AllocMatriceDistance(int_mapSize);
 
     srand(time(NULL)); //init RNG
-    matrice_Map = GenerateMap(matrice_Map, int_mapSize, float_diffRate, &s_playerInfo_player);
-    // matrice_Distance = Generatematrice_Distance(int_mapSize, matrice_Distance);
+    
+    int bool_mapDoable = 0;
+    while(!bool_mapDoable)  //Generation d'une Map Faisable
+    {
+        matrice_Map = InitMatriceMap(matrice_Map, int_mapSize);
+        matrice_Map = GenerateMap(matrice_Map, int_mapSize, float_diffRate, &s_playerInfo_player);
+        bool_mapDoable = CheckMapDoable(matrice_Map, s_playerInfo_player.int_x, s_playerInfo_player.int_y, int_mapSize);
+    }
+    
+    matrice_Distance = GenerateMatriceDistance(int_mapSize, matrice_Distance);
 
     DisplayMap(matrice_Map, int_mapSize);
-    // DisplayMatriceDistance(int_mapSize, matrice_Distance);
+    // DebugDisplayMatriceDistance(int_mapSize, matrice_Distance);
     printf("Votre energie : %d\n",s_playerInfo_player.int_energy);
 
     int key_pressed;
