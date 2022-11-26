d47c1bd7179fb38dda74cd0a3eb2394af4266291
LegJo <106883191+LegJo@users.noreply.github.com>
2022-11-26 19:01:56 +0100

    Update InitGame.c

--- src/InitGame.c
+++ src/InitGame.c
@@ -327,45 +327,47 @@ int*** GenerateMatriceDistance(int int_mapSize, int*** matrice_Distance)
 
 }
 
-int CheckPath(int** matrice_Map, int int_Coordx, int int_Coordy, int int_maxCoord, int int_Start) //verifie si la matrice map generer possede un chemin faisable recursivement; 
+int CheckPath(int** matrice_Map, int int_Coordx, int int_Coordy, int int_maxCoord, int int_Start, int int_remainEnergy) //verifie si la matrice map generer possede un chemin faisable recursivement; 
 {
-    
-    if( !IsBetween(int_Coordx, 0, int_maxCoord) || !IsBetween(int_Coordy, 0, int_maxCoord)){
+    if(int_remainEnergy <= 0){
         return 0;
-    }
-    else if(CoordCompare(matrice_Map, int_Coordx, int_Coordy, REP_END)){
+    } else if( !IsBetween(int_Coordx, 0, int_maxCoord) || !IsBetween(int_Coordy, 0, int_maxCoord)){
+        return 0;
+    } else if(CoordCompare(matrice_Map, int_Coordx, int_Coordy, REP_END)){
         return 1;
-    }else if(CoordCompare(matrice_Map, int_Coordx, int_Coordy, REP_OBSTACLE1) || CoordCompare(matrice_Map, int_Coordx, int_Coordy, REP_OBSTACLE2)){
+    } else if(CoordCompare(matrice_Map, int_Coordx, int_Coordy, REP_OBSTACLE1) || CoordCompare(matrice_Map, int_Coordx, int_Coordy, REP_OBSTACLE2)){
         return 0;
-    }else{
+    } else if(CoordCompare(matrice_Map, int_Coordx, int_Coordy, REP_END)){
+        int_remainEnergy += GAIN_ENERGY;
+    } else {
         switch(int_Start)
         {
             case 1: //le joueur à demarrer en 0,0
                 return (
-                    CheckPath(matrice_Map, int_Coordx + 1, int_Coordy, int_maxCoord, int_Start )
-                    || CheckPath(matrice_Map, int_Coordx, int_Coordy + 1, int_maxCoord, int_Start )
-                    || CheckPath(matrice_Map, int_Coordx + 1, int_Coordy + 1, int_maxCoord, int_Start )
+                    CheckPath(matrice_Map, int_Coordx + 1, int_Coordy, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
+                    || CheckPath(matrice_Map, int_Coordx, int_Coordy + 1, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
+                    || CheckPath(matrice_Map, int_Coordx + 1, int_Coordy + 1, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
                 );
                 break;
             case 2: //le joueur à demarrer en 0,mapSize
                 return (
-                    CheckPath(matrice_Map, int_Coordx + 1, int_Coordy, int_maxCoord, int_Start  )
-                    || CheckPath(matrice_Map, int_Coordx, int_Coordy - 1, int_maxCoord, int_Start )
-                    || CheckPath(matrice_Map, int_Coordx + 1, int_Coordy - 1, int_maxCoord, int_Start )
+                    CheckPath(matrice_Map, int_Coordx + 1, int_Coordy, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
+                    || CheckPath(matrice_Map, int_Coordx, int_Coordy - 1, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
+                    || CheckPath(matrice_Map, int_Coordx + 1, int_Coordy - 1, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
                 );
                 break;
             case 3: //le joueur à demarrer en mapSize,0
                 return (
-                    CheckPath(matrice_Map, int_Coordx - 1, int_Coordy, int_maxCoord, int_Start  )
-                    || CheckPath(matrice_Map, int_Coordx, int_Coordy + 1, int_maxCoord, int_Start )
-                    || CheckPath(matrice_Map, int_Coordx - 1, int_Coordy + 1, int_maxCoord, int_Start )
+                    CheckPath(matrice_Map, int_Coordx - 1, int_Coordy, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY  )
+                    || CheckPath(matrice_Map, int_Coordx, int_Coordy + 1, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
+                    || CheckPath(matrice_Map, int_Coordx - 1, int_Coordy + 1, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
                 );
                 break;
             case 4: //le joueur à demarrer en mapSize, mapSize
                 return (
-                       CheckPath(matrice_Map, int_Coordx - 1, int_Coordy, int_maxCoord, int_Start  )
-                    || CheckPath(matrice_Map, int_Coordx, int_Coordy - 1, int_maxCoord, int_Start )
-                    || CheckPath(matrice_Map, int_Coordx - 1, int_Coordy - 1, int_maxCoord, int_Start )
+                       CheckPath(matrice_Map, int_Coordx - 1, int_Coordy, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY  )
+                    || CheckPath(matrice_Map, int_Coordx, int_Coordy - 1, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
+                    || CheckPath(matrice_Map, int_Coordx - 1, int_Coordy - 1, int_maxCoord, int_Start, int_remainEnergy - LOST_ENERGY )
                 );
                 break;
         }
@@ -374,24 +376,47 @@ int CheckPath(int** matrice_Map, int int_Coordx, int int_Coordy, int int_maxCoor
     }
 }
 
-int CheckMapDoable(int** matrice_Map, int int_CoordPlayer_x,  int int_CoordPlayer_y, int int_mapSize) //cherche si un chemin est faisable en fonction du placement du player au debut
+int CheckMapDoable(int** matrice_Map, int int_CoordPlayer_x,  int int_CoordPlayer_y, int int_mapSize, int int_baseEnergy) //cherche si un chemin est faisable en fonction du placement du player au debut
 {
     int int_maxCoord = int_mapSize - 1;
     if(int_CoordPlayer_x == 0){
         if(int_CoordPlayer_y == 0){
-            return CheckPath(matrice_Map, 0, 0, int_maxCoord, 1);
+            return CheckPath(matrice_Map, 0, 0, int_maxCoord, 1, int_baseEnergy);
         }
         else {
-            return CheckPath(matrice_Map, 0, int_maxCoord, int_maxCoord, 2);
+            return CheckPath(matrice_Map, 0, int_maxCoord, int_maxCoord, 2, int_baseEnergy);
         }
     } else {
         if(int_CoordPlayer_y == 0){
-            return CheckPath(matrice_Map, int_maxCoord, 0, int_maxCoord, 3);
+            return CheckPath(matrice_Map, int_maxCoord, 0, int_maxCoord, 3, int_baseEnergy);
         }
         else {
-            return CheckPath(matrice_Map, int_maxCoord, int_maxCoord, int_maxCoord, 4);
+            return CheckPath(matrice_Map, int_maxCoord, int_maxCoord, int_maxCoord, 4, int_baseEnergy);
         }
     }
 }
 
+int** InitMap(int int_mapSize, float float_diffRate, PlayerInfo* p_playerInfo_player)
+{
+    int** matrice_Map = AllocMatriceMap(int_mapSize); // Allocation de la Matrice Map
+    int bool_mapDoable = 0;
+    while(!bool_mapDoable)  //Generation d'une Map Faisable
+    {
+        matrice_Map = InitMatriceMap(matrice_Map, int_mapSize);
+        matrice_Map = GenerateMap(matrice_Map, int_mapSize, float_diffRate, p_playerInfo_player);
+        bool_mapDoable = CheckMapDoable(matrice_Map,
+                                        p_playerInfo_player->int_x,
+                                        p_playerInfo_player->int_y,
+                                        int_mapSize, 
+                                        p_playerInfo_player->int_energy);
+    }
 
+    return (matrice_Map);
+}
+
+int*** InitDistance(int int_mapSize)
+{
+    int*** matrice_Distance= AllocMatriceDistance(int_mapSize);
+    matrice_Distance = GenerateMatriceDistance(int_mapSize, matrice_Distance);
+    return (matrice_Distance);
+}
