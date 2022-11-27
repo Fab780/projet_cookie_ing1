3c59efaab699496e100bf775cf9738b1ef9de3ec
JordanG2 <115002419+JordanG2@users.noreply.github.com>
2022-11-27 16:38:19 +0100

    Add files via upload

--- include/Miscellanous.h
+++ include/Miscellanous.h
@@ -8,10 +8,20 @@
     #include <string.h>
     #include <time.h>   
 
-    typedef struct PlayerInfo PlayerInfo; 
-    struct PlayerInfo{ // Structure contennant les informations du joueur
+    typedef struct coordonnees coordonnees;
+    struct coordonnees{ // Structure contenant les coordonnées du joueur 
         int int_x;
         int int_y;
+    };
+
+    typedef struct Node{ // enregistre les coordonnées dans une pile
+        coordonnees coordonnees_player;
+        struct Node *next;
+    }List, Node;
+
+    typedef struct PlayerInfo PlayerInfo; 
+    struct PlayerInfo{ // Structure contennant les informations du joueur
+        coordonnees coordonnees_player;
         int int_energy;
         int int_distance;
         int int_gain_energy;
@@ -28,5 +38,21 @@
     void printf_center(const char* str);
 
     void InitRNG();
+    
+    Node *createNode(coordonnees coordonnees_player);
+
+    List *emptyList(void);
+
+    int isEmpty(List *List_L);
+
+    long lengthList(List *List_L);
+
+    List *addnode(List *List_L, coordonnees coordonnees_player);
+
+    List *freeLastCoord(List *List_L);
+
+    List *freeList(List *List_L);
+
+    void printList(List *List_L);
 
 #endif
